d1306a9b299b8d315618282b4b9d430079ab7a0c
QA <QAtest@email.com>
2023-11-16 16:38:41 +0000

    maven

---
+++ DemoMavenPro/src/main/java/com/wipro/Nov11/App.java
@@ -0,0 +1,13 @@
+package com.wipro.Nov11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
